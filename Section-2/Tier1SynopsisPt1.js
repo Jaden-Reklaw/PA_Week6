caab4770cd3d7a1d35d4207ff087aec5b4e983b8
Jordan Walker <jwalker117@gmail.com>
2020-02-29 17:46:59 -0600

    Adding W6 Video 4 Notes

--- Section-2/Tier1SynopsisPt1.js
+++ Section-2/Tier1SynopsisPt1.js
@@ -1,6 +1,7 @@
 /*
-	Tier 1 Synopsis Part 1
+	Tier 1 Synopsis Part 1 and Part 2
 
+	Part 1
 	Create and index.html - Default HTML
 	Connect this Tier1SynopsisPt1.js to it
 	Source JQuery using CDN
@@ -10,6 +11,11 @@
 	One Button that on click adds to a list
 	list of the items add
 	Remaining Budget
+
+	Part 2
+	Calculate Remaining Budget
+	Display list items and purchase price
+
 */
 
 //Test Connection
@@ -32,6 +38,37 @@ const addPurchase = () => {
 	$('#item').val('');
 	$('#price').val('');
 	//calculate remaining budget
+	calculateRemainingBudget();
+	//Update DOM
+	displayPurchases();
+}
+
+const calculateRemainingBudget = () => {
+	//loop through purchase array
+	let totalPrice = 0;
+	for(let i = 0; i < purchases.length; i++) {
+		//for each purchase, add up total of all prices
+		totalPrice += Number(purchases[i].price);
+	}
+	console.log("total price:", totalPrice);
+	//subtract total prices from budget for remainingBudget
+	const remainingBudget = budget - totalPrice;
+	//display remainingBudget
+	let el = $('#remainingBudget');
+	el.empty();
+	el.append(remainingBudget);
+}
+
+const displayPurchases = () => {
+	console.log('in displayPurchases');
+	//target output by ID
+	let el = $('#purchasesOut');
+	//Empty
+	el.empty();
+	//loop through purchases array
+	for(let purchase of purchases) {
+		el.append(`<li> ${purchase.name}: $${purchase.price}</li>`);
+	}
 }
 
 const readyNow = () => {
