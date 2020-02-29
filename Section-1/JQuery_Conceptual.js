780344872b0ba18b32855e4b323989fdc561fdec
Jordan Walker <jwalker117@gmail.com>
2020-02-29 11:23:31 -0600

    Modified Week 6 Video 1 Notes

---
+++ Section-1/JQuery_Conceptual.js
@@ -0,0 +1,12 @@
+/*
+	JQuery Conceptual
+	HTML - structure/Web Page
+	CSS - Styles
+	Javascript - Functionality
+	JQuery - Library that expands the abilities of Javascript
+
+	JQuery makes it easier to manipulate the DOM, select elements and functions
+	to set and get values.
+	You must source your JQuery file or reference before any code that uses 
+	JQuery.
+*/
